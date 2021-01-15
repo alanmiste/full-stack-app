b00572013e90ac39b646d657cb368af18360ecf8
alanmiste <baranalaan@gmail.com>
2021-01-15 14:15:09 +0100

    read the state onclick and post to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 
 function App() {
   const [name, setname] = useState("")
   const [todos, settodos] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:4000/todos")
-    .then(res => res.json())
-    .then(res =>    { 
-      settodos(res)
-      console.log(res)
-    })
-  },[])
+      .then(res => res.json())
+      .then(res => {
+        settodos(res)
+        console.log(res)
+      })
+  }, [])
   const [users, setusers] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-    .then(res => res.json())
-    .then(res =>    { 
-      setusers(res)
-      console.log(res)
-    })
-  },[])
+      .then(res => res.json())
+      .then(res => {
+        setusers(res)
+        console.log(res)
+      })
+  }, [])
   console.log(name);
   return (
     <div className="App">
@@ -38,23 +38,37 @@ function App() {
         >
           Learn React
         </a>
-        <input 
-        id="searchuser" 
-        placeholder="Create user with name"
-        // controlled component needs a controlled value from getter
-        value={name}
-        // controlled component needs a setter
-        onChange={(event)=> {
-          setname(event.target.value)
-        }}
+        <input
+          id="searchuser"
+          placeholder="Create user with name"
+          // controlled component needs a controlled value from getter
+          value={name}
+          // controlled component needs a setter
+          onChange={(event) => {
+            setname(event.target.value) // set the state variable to the current input value
+          }}
         />
-        <button>Submit</button>
-        {todos.map(todo=>(
+        <button
+          onClick={(event) => { // react evetn  handler for click
+            fetch('http://localhost:4000/todos', {
+              method: 'post', // defining the fetch request as a POST request
+              body: JSON.stringify({"title": name}),// THE PAYLOAD / DATA / New Todo you send to the server as POST request
+              headers: {
+                "content-type": "application/json" // required for that the post request is accepted in the backend as JSON request. So you can access the req.body and its not empty.
+              }
+            }).then(function (response) {
+              return response.json();
+            }).then(function (data) {
+              console.log('Created Gist:', data);
+            });
+          }}
+        >Submit</button>
+        {todos.map(todo => (
           <div>
             {todo.title}
           </div>
         ))}
-        {users.map(user=>(
+        {users.map(user => (
           <div>
             {user.name}
           </div>
